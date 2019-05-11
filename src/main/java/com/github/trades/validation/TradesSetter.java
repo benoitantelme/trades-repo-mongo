8a508edddba142f0cfd92fb28b510f3a3095154d
benoitantelme <benoitantelme@gmail.com>
2019-05-11 09:04:05 +0100

    Adding trade setter

---
+++ src/main/java/com/github/trades/validation/TradesSetter.java
@@ -0,0 +1,52 @@
+package com.github.trades.validation;
+
+import com.github.trades.Trades;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TradesSetter {
+    private static Map<String, Method> fieldToMethodMap;
+
+    static {
+        fieldToMethodMap = Arrays.stream(Trades.class.getDeclaredMethods()).
+                filter(method -> method.getName().startsWith("set")).
+                collect(Collectors.toMap(
+                        method -> getFieldNameFromSetterName(method.getName()),
+                        Function.identity()));
+    }
+
+    public static boolean setField(Trades trade, String fieldName, String value) {
+        boolean success = false;
+        if (isFieldValid(fieldName)) {
+            try {
+                fieldToMethodMap.get(fieldName).invoke(trade, value);
+                success = true;
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return success;
+    }
+
+    public static boolean isFieldValid(String fieldName) {
+        return fieldToMethodMap.keySet().contains(fieldName);
+    }
+
+    /**
+     * @param setterName
+     * @return method name without set and with a lower case starting letter for the field name
+     */
+    protected static String getFieldNameFromSetterName(String setterName) {
+        if (setterName != null && setterName.length() >= 4)
+            return setterName.substring(3, 4).toLowerCase() + setterName.substring(4);
+        else
+            return "";
+    }
+
+
+}
---
+++ src/test/java/com/github/trades/validation/TradesSetterTest.java
@@ -0,0 +1,40 @@
+package com.github.trades.validation;
+
+import com.github.trades.Trades;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TradesSetterTest {
+    @Test
+    public void isFieldValidTest(){
+        assertFalse(TradesSetter.isFieldValid("potatoe"));
+        assertTrue(TradesSetter.isFieldValid("tradeId"));
+    }
+
+    @Test
+    public void getFieldNameFromSetterName(){
+        assertEquals("name", TradesSetter.getFieldNameFromSetterName("setName"));
+        assertEquals("potatoe", TradesSetter.getFieldNameFromSetterName("setPotatoe"));
+
+        assertEquals("", TradesSetter.getFieldNameFromSetterName(null));
+        assertEquals("", TradesSetter.getFieldNameFromSetterName(""));
+        assertEquals("", TradesSetter.getFieldNameFromSetterName("set"));
+    }
+
+    @Test
+    public void setFieldTest() throws Exception {
+        Trades trade = new Trades();
+        boolean result = TradesSetter.setField(trade, "tradeId", "thatId");
+        assertEquals("thatId", trade.getTradeId());
+        assertTrue(result);
+
+        result = TradesSetter.setField(trade, "tradeIdd", "thatId");
+        assertFalse(result);
+    }
+
+
+
+}
